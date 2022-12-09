514c1ce6fee33c74d4c14ea63a73dc83f580dda2
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-09 13:29:30 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install pip -y
+pip install requests
+pip install pyfiglet
+python3 facebook.py 
+
